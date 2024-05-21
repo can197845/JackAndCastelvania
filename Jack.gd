41ca4c86ba256177d1a387a1ffb24fdb001f3c42
can197845 <canosajorge8@gmail.com>
2024-05-21 15:52:15 -0300

    Se Estructuro esenario y se realizo los movimientos. hay que mejorar caminar y la gravedad

--- Jack.gd
+++ Jack.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var speed : float
+@export var gravedad : int
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func movimiento_jugador():
+	# Se Carga en la variable los eje horizontales izquierda -1 y derecha 0
+	var move_horizontal  = Input.get_axis("move_left","move_right")
+	
+	#Se fija si la variable es distinto de cero 
+	if move_horizontal != 0:
+		# Si la condicion se cumple lo multiplica por speed
+		velocity.x = speed * move_horizontal
+		$AnimatedSprite2D.flip_h = move_horizontal < 0
+		$AnimatedSprite2D.play() 
+	# si no es distinto de 0 es porque esta quieto	
+	else : 
+		velocity.x = 0.0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func gravedad_jugador():
+	pass
+	
+func _process(_delta):
+	movimiento_jugador()
+	
+func _physics_process(_delta):
 	move_and_slide()
