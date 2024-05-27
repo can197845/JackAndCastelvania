27bbae4afe6922ed8b1a59e9b50dce866fde6dd8
can197845 <canosajorge8@gmail.com>
2024-05-27 11:51:07 -0300

    problemas con la sierra se frena cuando detecta que choca con mi player. lo que tendria que hjacer es que pase de largo

--- sierra.gd
+++ sierra.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
+#AGREGAMOS LAS VARIABLES PARA MANEJAR LA POSICION DE LA SIERRA
 const VELOCIDAD = 200
+const RAY_PISO_POSICION_X = 20
+const RAY_PARED_TARGET_X = 20
 @export var gravedad : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-#AGREGAMOS LAS VARIABLES PARA MANEJAR LA POSICION DE LA SIERRA
-const RAY_PISO_POSICION_X = 21
-const RAY_PARED_TARGET_X = 20
 
 func _ready():
 	velocity.x = VELOCIDAD
@@ -16,11 +16,11 @@ func _physics_process(delta):
 	# Agrego la gravedad al objeto
 	
 	if not is_on_floor():
-		velocity.y = gravedad* delta
+		velocity.y = gravedad * delta
 	
 	if not $RayCastDetecSuelo.is_colliding() || $RayCastDetecPared.is_colliding():
 		velocity.x *=-1
 		$RayCastDetecSuelo.position.x *=-1
-		$RayCastDetecPared.target_position.x *=-1 		
+		$RayCastDetecPared.target_position.x *=-1
 	
 	move_and_slide()
